27c1b29bb8e55ef6e8ba1f5ce575d2ba57cd5e94
forthespada <forthespada@foxmail.com>
2021-08-16 09:46:16 +0800

    111

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -91,7 +91,7 @@
       methods:{
           async getCateList(){
             const {data:res} = await this.$http.get('categories')
-            if(res.meta.status !== 200){
+            if(res.metgita.status !== 200){
               return this.$message.error('获取商品列表失败')
             }
             this.cateList = res.data
